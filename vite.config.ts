7c43ac818a723d072d02d7ecc72cfbd7ca98ccd0
tem <artemmalinin173@gmail.com>
2026-04-30 14:45:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-//import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  // ЗАМЕНИТЕ 'название-вашего-репозитория' на реальное имя вашего проекта на GitHub
+  // Например, если ссылка https://github.com/sotrymore/my-game, то base: '/my-game/'
+  base: 'sotrymore.github.io', 
+  
   plugins: [
     react(),
     tailwindcss(),
-    //viteSingleFile()
   ],
   resolve: {
     alias: {
