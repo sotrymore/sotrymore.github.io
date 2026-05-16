fc5c1ae209f36affdf3df4a39e6494227026384a
tem <artemmalinin173@gmail.com>
2026-05-16 12:10:41 +0300

    финальная версия

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,22 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import basicSsl from "@vitejs/plugin-basic-ssl"; // <--- Добавляем импорт
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/', // ТАК КАК ЭТО ГЛАВНЫЙ ДОМЕН .github.io
   plugins: [
-    react(),
-    tailwindcss(),
+    react(), 
+    tailwindcss(), 
+    //viteSingleFile(), 
+    basicSsl() // <--- Добавляем плагин в список
   ],
+  server: {
+    https: true, // <--- Принудительно включаем режим HTTPS
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
